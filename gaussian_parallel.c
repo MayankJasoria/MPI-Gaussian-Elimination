e036a30bcde27c54d302366845bc56bf2fc28db9
MayankJasoria <jasoriamayank@gmail.com>
2020-03-26 00:05:25 +0530

    Removed debug messages

--- gaussian_parallel.c
+++ gaussian_parallel.c
@@ -15,19 +15,8 @@
  * @param rows_per_proc The total number of rows present within current process
  * @param num_proc      The total number of processes
  */
-void perform_elimination(int recv_id, int id, int num_eq, double* proc_rows, double* proc_vals, int curr, double* recvd_row, int rows_per_proc, int num_proc, int * var_perm, FILE* debugfp) {
-    /* update indices of each variable, (to be used during back substitution) */
-    
-    /* DEBUG */
-    fprintf(debugfp, "recv_id = %d, id = %d, curr = %d, piv_index = %d, pivot_val = %lf\n", recv_id, id, curr, (int) recvd_row[num_eq], recvd_row[recv_id]);
-    fprintf(debugfp, "BEFORE PROCESSING ELIMINATION\n");
-    for(int i = 0; i < rows_per_proc; i++) {
-        for(int j = 0; j < num_eq; j++) {
-            fprintf(debugfp, " %lf ", proc_rows[(i * num_eq) + j]);
-        }
-        fprintf(debugfp, "\n");
-    }
-    /* DEBUG ENDS */
+void perform_elimination(int recv_id, int id, int num_eq, double* proc_rows, double* proc_vals, int curr, double* recvd_row, int rows_per_proc, int num_proc, int * var_perm) {
+    /* update indices of each variable */
     int piv_idx = (int) recvd_row[num_eq];
     int tmp = var_perm[piv_idx];
     var_perm[piv_idx] = var_perm[recv_id];
@@ -50,14 +39,6 @@ void perform_elimination(int recv_id, int id, int num_eq, double* proc_rows, dou
             proc_vals[i] -= recvd_row[num_eq + 1] * piv_val;
         }
     }
-
-    fprintf(debugfp, "\nAFTER PROCESSING ELIMINATION\n");
-    for(int i = 0; i < rows_per_proc; i++) {
-        for(int j = 0; j < num_eq; j++) {
-            fprintf(debugfp, " %lf ", proc_rows[(i * num_eq) + j]);
-        }
-        fprintf(debugfp, "\n");
-    }
 }
 
 /**
@@ -69,11 +50,10 @@ void perform_elimination(int recv_id, int id, int num_eq, double* proc_rows, dou
  *
  * @return The index of the pivot element
  */
-int compute_pivot(int curr, int num_proc, int num_eq, double* proc_rows, FILE* debugfp) {
+int compute_pivot(int curr, int num_proc, int num_eq, double* proc_rows) {
     double mx = -1;
     int pivot;
     int row_id = curr / num_proc;
-    fprintf(debugfp, "Computing pivot: curr = %d\n", curr);
     for(int i = curr; i < num_eq; i++) {
         double val = proc_rows[(row_id * num_eq) + i];
         if(val < 0)
@@ -82,7 +62,6 @@ int compute_pivot(int curr, int num_proc, int num_eq, double* proc_rows, FILE* d
             mx = abs(val);
             pivot = i;
         }
-        fprintf(debugfp, "val = %lf, pivot = %d\n", val, pivot);
     }
     return pivot;
 }
@@ -211,26 +190,6 @@ int main(int argc, char** argv) {
     }
     MPI_Scatterv(val_mat, divs, displs, MPI_DOUBLE, proc_vals, rows_per_proc, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    /* DEBUG */
-    char fname[11];
-    strcpy(fname, "debug");
-    fname[5] = ('0' + (char)id);
-    fname[6] = '.';
-    fname[7] = 't';
-    fname[8] = 'x';
-    fname[9] = 't';
-    fname[10] = '\0';
-    FILE* debugfp = fopen(fname, "w");
-    for(int i = 0; i < rows_per_proc; i++) {
-        for(int j = 0; j < num_eq; j++) {
-            fprintf(debugfp, "%lf ", proc_rows[(i * num_eq) + j]);
-        }
-      fprintf(debugfp, "%lf\n", proc_vals[i]);
-    //    for(int j = 0; j < num_eq; j++) {
-    //        fprintf(debugfp, "%lf ", eq_mat[i][j]);
-    //    }
-        // fprintf(debugfp, "\n");
-    }
     MPI_Barrier(MPI_COMM_WORLD);
     /* DEBUG ENDS */
     
@@ -254,20 +213,11 @@ int main(int argc, char** argv) {
             MPI_Recv(recvd_row, num_eq + 2, MPI_DOUBLE, prev_proc, i, MPI_COMM_WORLD, &st);
             if(curr - i < num_proc - 1)
                 MPI_Send(recvd_row, num_eq + 2, MPI_DOUBLE, next_proc, i, MPI_COMM_WORLD);
-            fprintf(debugfp, "\nReceived rows\n");
-            for(int k = 0; k < num_eq + 2; k++) {
-                fprintf(debugfp, "%lf ", recvd_row[k]);
-            }
-            perform_elimination(i, id, num_eq, proc_rows, proc_vals, curr, recvd_row, rows_per_proc, num_proc, var_perm, debugfp);  // eliminates from current row till last row in process
+            perform_elimination(i, id, num_eq, proc_rows, proc_vals, curr, recvd_row, rows_per_proc, num_proc, var_perm);
         }
-        piv = compute_pivot(curr, num_proc, num_eq, proc_rows, debugfp); 
-        fprintf(debugfp, "\nPivots\n");
-        fprintf(debugfp, "%d %d\n", curr, piv);
+        piv = compute_pivot(curr, num_proc, num_eq, proc_rows); 
         perform_division(id, curr, proc_rows, piv, num_proc, num_eq, rows_per_proc, proc_vals);
-        fprintf(debugfp, "\nAfter Division Row no. : %d\n", curr);
-        for(int k = 0; k < num_eq + 2; k++) {
-            fprintf(debugfp, "%lf ", proc_rows[cnt*num_eq + k]);
-        }
+        
         double send_buf[num_eq + 2];
         for(int j = 0; j < num_eq; j++) {
             send_buf[j] = proc_rows[(cnt * num_eq) + j];
@@ -285,11 +235,9 @@ int main(int argc, char** argv) {
         curr += num_proc;
         
         if(curr < num_eq) {
-            perform_elimination(prev_curr, id, num_eq, proc_rows, proc_vals, curr, send_buf, rows_per_proc, num_proc, var_perm, debugfp);
+            perform_elimination(prev_curr, id, num_eq, proc_rows, proc_vals, curr, send_buf, rows_per_proc, num_proc, var_perm);
         }
     }
-
-    fflush(debugfp);
 
     MPI_Barrier(MPI_COMM_WORLD);
 
@@ -298,44 +246,21 @@ int main(int argc, char** argv) {
     int prev_lst = num_eq;
     double x_val;
     int count = 1;
-    fprintf(debugfp, "prev_lst = %d, lst = %d\n", prev_lst, lst);
-    fflush(debugfp);
     for(int cnt = rows_per_proc - 1; cnt >= 0; cnt--) {
-        fprintf(debugfp, "1. x_val = %lf, cnt = %d, lst = %d, prev_lst = %d\n", x_val, cnt, lst, prev_lst);
-        fflush(debugfp);
         for(int i = prev_lst - 1; i > lst; i--) {
-            fprintf(debugfp, "2. x_val = %lf, cnt = %d, i = %d, lst = %d, prev_lst = %d\n", x_val, cnt, i, lst, prev_lst);
-            fflush(debugfp);
             MPI_Recv(&x_val, count, MPI_DOUBLE, next_proc, i + num_eq, MPI_COMM_WORLD, &st);
-            fprintf(debugfp, "3. x_val = %lf, cnt = %d, i = %d, lst = %d, prev_lst = %d\n", x_val, cnt, i, lst, prev_lst);
-            fflush(debugfp);
             if(lst - i < num_proc - 1) {
                 MPI_Send(&x_val, count, MPI_DOUBLE, prev_proc, i + num_eq, MPI_COMM_WORLD);
             }
-            
-            fprintf(debugfp, "Proc_vals: ");
-            fflush(debugfp);
             proc_vals[lst] -= proc_rows[(cnt * num_eq) + i] * x_val;
             for(int k = lst - num_proc; k >= 0; k -= num_proc) {
                 proc_vals[k] -= proc_rows[(k * num_eq) + i] * x_val;
             }
-            
-            for(int i = 0; i < num_eq; i++) {
-                fprintf(debugfp, "%lf ", proc_vals[i]);
-            }
-            fprintf(debugfp, "\n\n");
-            fflush(debugfp);
         }
         double ans = proc_vals[cnt];
         for(int k = lst - num_proc; k >= 0; k -= num_proc) {
             proc_vals[k] -= proc_rows[(k * num_eq) + lst] * x_val;
         }
-
-        fprintf(debugfp, "\nPrinting PROC VALS\n");
-        for(int i = 0; i < rows_per_proc; i++) {
-            fprintf(debugfp, "%lf ", proc_vals[i]);
-        }
-        fprintf(debugfp, "\n");
 
         if(lst > 0) {
             /* We don't want to send first pivot */
@@ -344,33 +269,7 @@ int main(int argc, char** argv) {
         prev_lst = lst;
         lst -= num_proc;
     }
-    
-    fprintf(debugfp, "\nFinal\n");
-    for(int i = 0; i < rows_per_proc; i++) {
-        for(int j = 0; j < num_eq; j++) {
-            fprintf(debugfp, "%lf ", proc_rows[(i * num_eq) + j]);
-        }
-      fprintf(debugfp, "%lf\n", proc_vals[i]);
-    //    for(int j = 0; j < num_eq; j++) {
-    //        fprintf(debugfp, "%lf ", eq_mat[i][j]);
-    //    }
-        // fprintf(debugfp, "\n");
-    }
-    
-    fprintf(debugfp, "\n\nVariables Permutation:\n");
-    for(int i = 0; i < num_eq; i++) {
-        fprintf(debugfp, "%d ", var_perm[i]);
-    }
-    fprintf(debugfp, "\n");
-    
-    fprintf(debugfp, "\nPrinting PROC VALS\n");
-    for(int i = 0; i < rows_per_proc; i++) {
-        fprintf(debugfp, "%lf ", proc_vals[i]);
-    }
-    fprintf(debugfp, "\n");
 
-    fclose(debugfp);
-    
     double res[num_eq];
     
     int recv_proc = num_eq % num_proc;
@@ -397,7 +296,7 @@ int main(int argc, char** argv) {
         
         /* print output */
         FILE* outfile = fopen("output.txt", "w");
-        fprintf(outfile, "The solution of the system of equations is {\n");
+        fprintf(outfile, "The solution of the system of equations is \n{");
         for(int i = 0; i < num_eq; i++) {
             fprintf(outfile, "%lf ", solution[i]);
         }
